e63160313afe66bc020eea32d1b995d753c8c554
MichaelMichael501 <126837822+MichaelMichael501@users.noreply.github.com>
2023-07-05 16:39:34 +0800

    update authentication

--- include/authentication.php
+++ include/authentication.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Check if the user is logged in
-if (!isset($_SESSION['logged_in'])) {
+if ($_SESSION['auth'] != session_id()) {
     // Redirect the user to the login page or any other unauthorized page
     header("Location: login.php"); // Replace "login.php" with the appropriate page URL
     exit();
