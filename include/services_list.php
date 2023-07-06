37e258b8a6040136813fe591b30f3b9b5f0c97ef
MichaelMichael501 <126837822+MichaelMichael501@users.noreply.github.com>
2023-07-06 11:12:13 +0800

    change code again

--- include/services_list.php
+++ include/services_list.php
@@ -3,16 +3,17 @@ include('../process/config.php');
 $typeId = $_POST['typeId'];
 if ($typeId) {
   /***Calling Table of Industry****************************************************/
-  $industryQuery = @"SELECT * FROM services";
+  $industryQuery = "SELECT * FROM services";
   $result = mysqli_query($con, $industryQuery);
   /*******************************************************/
   echo '<ul class="list-services">';
   while ($row = mysqli_fetch_assoc($result)) {
-    $devices = mysqli_query($con, "SELECT * FROM `pricing` WHERE `services` = '" . $row['id'] . "' AND `type` = '$typeId'");
+    $deviceId = $row['id'];
+    $devices = mysqli_query($con, "SELECT * FROM `pricing` WHERE `services`='$deviceId' AND `type`='$typeId'");
     $value = mysqli_fetch_array($devices);
-    if ($value === null || $value['services'] != $row['id']) {
+    if ($value === null || $value['services'] != $deviceId) {
       echo '<li>';
-      echo '<input type="radio" id="selectedItems" name="selectedItems" value="' . $row['id'] . '">';
+      echo '<input type="radio" id="selectedItems" name="selectedItems" value="' . $deviceId . '">';
       echo $row['name'];
       echo '</li>';
     }
