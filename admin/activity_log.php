5fc50bede85d647f51f9c1717ff453c68040030a
MichaelMichael501 <126837822+MichaelMichael501@users.noreply.github.com>
2023-07-28 08:58:38 +0800

    new

---
+++ admin/activity_log.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php
+include('../include/include_body.php');
+include('../include/authentication.php');
+include('../process/config.php');
+?>
+<? ?>
+<html lang="en">
+
+<head>
+    <title>Add Services</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/pricing_service.css">
+    <link rel="stylesheet" type="text/css" href="../css/body.css">
+    <link rel="stylesheet" type="text/css" href="../css/add_services.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../js/add_type.js"></script>
+    <script src="../js/create_services.js"></script>
+</head>
+
+<body>
+    <?php echo $adminNavigation; ?>
+    <div class="container-add">
+      
+    </div>
+
+
+
+    <script src="../js/add_services.js"></script>
+
+</body>
+
+
+<footer class="all-footer">
+    <img src="../img/AyusinkophlogoPNG.png" alt="logo" class="head-logo">
+</footer>
